f130fc9728bfba98410343dfa706db6a01587f4d
kibinag0 <35554097+kibinag0@users.noreply.github.com>
2020-01-11 08:57:41 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,90 @@
+
+// Obtaining the length of drum elements
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+
+// let the sound on when the button has clicked
+
+for (var i = 0; i< numberOfButtons; i++){
+
+ document.querySelectorAll("button")[i].addEventListener("click", function () {
+
+   var innerWord = this.innerHTML;
+
+   makeSound(innerWord);
+   makeAnimation(innerWord);
+}
+);}
+
+
+// let the sound on when each key has pressed on the keyboard
+document.addEventListener("keydown",function(event){
+
+  var elementEvent = event["key"];
+
+  makeSound(elementEvent);
+  makeAnimation(elementEvent);
+
+});
+
+
+
+// Function that switch statement to allocate proper sound to each button
+function makeSound(key){
+   switch (key) {
+     case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+       break;
+
+     case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+       break;
+
+     case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+       break;
+
+     case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+       break;
+
+     case "j":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+       break;
+
+     case "k":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+       break;
+
+     case "l":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+       break;
+
+   default:
+      console.log("No sound");
+
+   }
+}
+
+
+// function that makes animation when makeSound() has in effect
+function makeAnimation(key){
+  var classSelected = document.querySelector("." + key)
+
+  // add class "pressed"
+  classSelected.classList.add("pressed");
+
+  // remove class "pressed" after 1 sec
+  setTimeout(function(){
+      classSelected.classList.remove("pressed");
+  }, 100)
+
+}
